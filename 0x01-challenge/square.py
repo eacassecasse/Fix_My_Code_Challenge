96199500ff6729ac9ec732e9075cc19aef1bef71
Edmilson Cassecasse <2015edmilsonb1327@gmail.com>
2024-07-24 11:06:14 +0200

    fix: Fit PEP8 Compliances

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
-class square():
-    
+
+class Square():
+
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """ Initializes a square """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -14,15 +15,18 @@ class square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
+        """ Permiter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ String representation of the square """
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
